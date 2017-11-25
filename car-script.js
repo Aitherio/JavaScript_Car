e84c67a5803bbd36553aaf4d821d9496ab0e53fb
Amy Hong <jhong81@my.bcit.ca>
2017-11-24 16:29:43 -0800

    deleted curves

--- car-script.js
+++ car-script.js
@@ -102,45 +102,6 @@ ctx.fill();
 
 // Smoke
 
-// from left-up to right-down
-// first line
-var xCoordinate1 = 410;
-var yCoordinate1 = 310;
-var radius1 = 280;
-var sAngle1 = 2.5;
-var eAngle1 = 3;
-
-ctx.beginPath();
-ctx.arc(xCoordinate1, yCoordinate1, radius1, sAngle1, eAngle1);
-ctx.stroke();
-
-
-
-var xCoordinate2 = -300;
-var yCoordinate2 = 400;
-var radius2 = 435;
-var sAngle2 = 1.2;
-var eAngle2 = 6.2;
-
-ctx.beginPath();
-ctx.arc(xCoordinate2, yCoordinate2, radius2, sAngle2, eAngle2);
-ctx.stroke();
-
-
-ctx.beginPath();
-ctx.moveTo(135, 340); // starting point
-ctx.quadraticCurveTo(155, 460, 210, 490);
-ctx.stroke();
-
-
-ctx.beginPath();
-ctx.moveTo(110, 100); // starting point
-ctx.quadraticCurveTo(135, 130, 135, 340);
-ctx.stroke();
-
-// second line // TODO: later
-
-
 /*
 // Smoke
 var canvas = document.getElementById("car-canvas");
